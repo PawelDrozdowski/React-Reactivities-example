5c115ec761ad4d376299cf1dda2ccfec05ee80f3
PawelDrozdowski <pawel.drozdowski.555@gmail.com>
2025-03-23 11:28:13 +0100

    init

---
+++ API/Program.cs
@@ -0,0 +1,42 @@
+using API.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+const string CORS_POLICY_NAME = "CorsPolicy";
+var builder = WebApplication.CreateBuilder(args);
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddApplicationServices(builder.Configuration, CORS_POLICY_NAME);
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseCors(CORS_POLICY_NAME);
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+//doing 'update-database' on every rerun
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedData(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error during migration");
+}
+
+app.Run();
